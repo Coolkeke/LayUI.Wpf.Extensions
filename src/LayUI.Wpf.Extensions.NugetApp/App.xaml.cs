717c8fd48c25b014dd73eacdaf7195eb291ce350
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2024-04-22 17:32:57 +0800

    完成Nuget上LayUI.WPF扩展插件翻译Demo演示

---
+++ src/LayUI.Wpf.Extensions.NugetApp/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using LayUI.Wpf.Extensions.NugetApp.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace LayUI.Wpf.Extensions.NugetApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
