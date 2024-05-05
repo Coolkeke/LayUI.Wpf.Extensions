58eb308b3cbd5da256d415cd0d46e4cd83252303
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2024-05-05 21:10:15 +0800

    增强多语言翻译兼容，支持动态读取字段翻译

--- src/LayUI.Wpf.Extensions/Language/LanguageExtension.cs
+++ src/LayUI.Wpf.Extensions/Language/LanguageExtension.cs
@@ -12,7 +12,6 @@ using System.Windows.Threading;
 using System.Xml.Linq;
 using System.ComponentModel;
 using System.Globalization;
-using System.Runtime.InteropServices;
 
 namespace LayUI.Wpf.Extensions
 {
@@ -45,7 +44,6 @@ namespace LayUI.Wpf.Extensions
         {
             _Key = key;
         }
-
         /// <summary>
         /// 绑定源
         /// </summary>
@@ -54,6 +52,11 @@ namespace LayUI.Wpf.Extensions
             get { return _Key; }
             set { _Key = value; }
         }
+        /// <summary>
+        /// 临时翻译数据源
+        /// </summary>
+        private static ResourceDictionary localSource;
+
         private static ResourceDictionary source;
         /// <summary>
         /// 翻译数据内存源数据
@@ -65,13 +68,26 @@ namespace LayUI.Wpf.Extensions
                 if (source == null) source = new ResourceDictionary();
                 return source;
             }
+            set
+            {
+                if (source != value)
+                {
+                    source = value;
+                    Refresh();
+                }
+            }
         }
+        /// <summary>
+        /// 判断初始化加载
+        /// </summary>
+        private static bool IsLoadedLanguage { get; set; }
+
         /// <summary>
         /// 获取内存中已加载的翻译结果
         /// </summary>
         /// <param name="key">唯一标识</param>
         /// <returns></returns>
-        public object GetValue(string key)
+        public static object GetValue(string key)
         {
             if (Source != null && Source.Contains(key)) return Source[key];
             return key;
@@ -110,15 +126,19 @@ namespace LayUI.Wpf.Extensions
         /// <param name="languageDictionary"></param>
         public static void LoadDictionary(ResourceDictionary languageDictionary)
         {
-            source = languageDictionary;
-            Refresh();
+            Source = languageDictionary;
         }
         /// <summary>
         /// 刷新页面可视化语言
         /// </summary>
         public static void Refresh()
         {
-            if (action != null) action.Invoke();
+            lock (Source)
+            {
+                if (action == null) return;
+                AddApplicationResourcesLanguage();
+                action.Invoke();
+            }
         }
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
@@ -126,70 +146,102 @@ namespace LayUI.Wpf.Extensions
             if (provideValueTarget.TargetObject.GetType().FullName == "System.Windows.SharedDp") return this;
             if (!(provideValueTarget.TargetObject is DependencyObject targetObject)) return this;
             if (!(provideValueTarget.TargetProperty is DependencyProperty targetProperty)) return this;
-            Action LanguageEvent = null;
-            LanguageEvent = () =>
-            {
-                BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key));
-            };
-            RoutedEventHandler loaded = null;
-            RoutedEventHandler unLoaded = null;
-            loaded = (o, e) =>
-            {
-                action += LanguageEvent;
-                if (o is FrameworkElement)
+            try
+            { 
+                Action LanguageEvent = null;
+                LanguageEvent = () => BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key)); ;
+                RoutedEventHandler loaded = null;
+                RoutedEventHandler unLoaded = null;
+                loaded = (o, e) =>
                 {
-                    (o as FrameworkElement).Loaded -= loaded;
-                    (o as FrameworkElement).Loaded += loaded;
-                }
-                if (o is FrameworkContentElement)
+                    action += LanguageEvent;
+                    if (o is FrameworkElement)
+                    {
+                        (o as FrameworkElement).Loaded -= loaded;
+                        (o as FrameworkElement).Loaded += loaded;
+                    }
+                    if (o is FrameworkContentElement)
+                    {
+                        (o as FrameworkContentElement).Loaded -= loaded;
+                        (o as FrameworkContentElement).Loaded += loaded;
+                    }
+                };
+                unLoaded = (o, e) =>
                 {
-                    (o as FrameworkContentElement).Loaded -= loaded;
-                    (o as FrameworkContentElement).Loaded += loaded;
-                }
-            };
-            unLoaded = (o, e) =>
-            {
-                action -= LanguageEvent;
-                if (o is FrameworkElement)
+                    action -= LanguageEvent;
+                    if (o is FrameworkElement)
+                    {
+                        (o as FrameworkElement).Unloaded -= unLoaded;
+                        (o as FrameworkElement).Unloaded += unLoaded;
+                    }
+                    else if (o is FrameworkContentElement)
+                    {
+                        (o as FrameworkContentElement).Unloaded -= unLoaded;
+                        (o as FrameworkContentElement).Unloaded += unLoaded;
+                    }
+                };
+                if (targetObject is FrameworkElement element)
                 {
-                    (o as FrameworkElement).Unloaded -= unLoaded;
-                    (o as FrameworkElement).Unloaded += unLoaded;
-                }
-                else if (o is FrameworkContentElement)
-                {
-                    (o as FrameworkContentElement).Unloaded -= unLoaded;
-                    (o as FrameworkContentElement).Unloaded += unLoaded;
-                }
-            };
-            if (targetObject is FrameworkElement element)
-            {
-                element.Loaded += loaded;
-                element.Unloaded += unLoaded;
-                DependencyPropertyChangedEventHandler elementDataContextChanged = null;
-                elementDataContextChanged += (o, e) =>
-                {
-                    element.DataContextChanged -= elementDataContextChanged;
+                    element.Loaded += loaded;
+                    element.Unloaded += unLoaded;
+                    DependencyPropertyChangedEventHandler elementDataContextChanged = null;
+                    elementDataContextChanged += (o, e) =>
+                    {
+                        element.DataContextChanged -= elementDataContextChanged;
+                        element.DataContextChanged += elementDataContextChanged;
+                        BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key));
+                    };
                     element.DataContextChanged += elementDataContextChanged;
-                    BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key));
-                };
-                element.DataContextChanged += elementDataContextChanged;
-            }
-            else if (targetObject is FrameworkContentElement contentElement)
-            {
-                contentElement.Loaded += loaded;
-                contentElement.Unloaded += unLoaded;
-                DependencyPropertyChangedEventHandler contentElementDataContextChanged = null;
-                contentElementDataContextChanged += (o, e) =>
+                }
+                else if (targetObject is FrameworkContentElement contentElement)
                 {
-                    contentElement.DataContextChanged -= contentElementDataContextChanged;
+                    contentElement.Loaded += loaded;
+                    contentElement.Unloaded += unLoaded;
+                    DependencyPropertyChangedEventHandler contentElementDataContextChanged = null;
+                    contentElementDataContextChanged += (o, e) =>
+                    {
+                        contentElement.DataContextChanged -= contentElementDataContextChanged;
+                        contentElement.DataContextChanged += contentElementDataContextChanged;
+                        BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key));
+                    };
                     contentElement.DataContextChanged += contentElementDataContextChanged;
-                    BindingOperations.SetBinding(targetObject, targetProperty, CreateBinding(Key));
-                };
-                contentElement.DataContextChanged += contentElementDataContextChanged;
+                }
+                if (!IsLoadedLanguage) IsLoadedLanguage = AddApplicationResourcesLanguage();
+                return CreateBinding(Key).ProvideValue(serviceProvider);
             }
-            return CreateBinding(Key).ProvideValue(serviceProvider);
+            catch (Exception ex)
+            {
+                throw new XamlParseException(ex.Message);
+            }
         }
-
+        /// <summary>
+        /// 给系统资源新增多语言文件
+        /// </summary>
+        private static bool AddApplicationResourcesLanguage()
+        {
+            try
+            {
+                var window = Application.Current?.MainWindow;
+                if (window != null && !DesignerProperties.GetIsInDesignMode(window))
+                {
+                    if (localSource == null) localSource = source;
+                    var lang = Application.Current.Resources?.MergedDictionaries?.Where(o => o.Source == localSource.Source)?.FirstOrDefault();
+                    if (lang != null) Application.Current.Resources?.MergedDictionaries?.Remove(lang);
+                    Application.Current.Resources?.MergedDictionaries?.Add(Source);
+                    localSource = Source;
+                }
+                return true;
+            }
+            catch { }
+            return false;
+        }
+        /// <summary>
+        /// 创建多语言绑定代码
+        /// </summary>
+        /// <param name="dependency"></param>
+        /// <param name="property"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         private MultiBinding CreateBinding(object key)
         {
             MultiBinding binding = new MultiBinding();
@@ -205,7 +257,7 @@ namespace LayUI.Wpf.Extensions
                 {
                     Source = key,
                     Mode = BindingMode.OneWay
-                }); 
+                });
             }
             binding.Bindings.Add(new Binding()
             {
